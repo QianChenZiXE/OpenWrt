e1b1408fc5d1c2b21894887265a3cdfdebcc2e56
QianChenZiXE <83319900+QianChenZiXE@users.noreply.github.com>
2025-11-04 16:39:38 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,9 @@ chmod +x /root/Clean_Temp.sh /root/PassWall.sh
 # 3. 写入定时任务（避免重复追加）
 CRON_CLEAN="0 0 * * * /bin/bash /root/Clean_Temp.sh"
 CRON_PASSW="*/10 * * * * /bin/bash /root/PassWall.sh"
+CRON_RAM="0 3 * * 0 env TZ=Asia/Shanghai /usr/bin/ram_release.sh release"
 
-for cron in "$CRON_CLEAN" "$CRON_PASSW"; do
+for cron in "$CRON_CLEAN" "$CRON_PASSW" "$CRON_RAM"; do
     grep -F "$cron" /etc/crontabs/root >/dev/null 2>&1 || echo "$cron" >> /etc/crontabs/root
 done
 
