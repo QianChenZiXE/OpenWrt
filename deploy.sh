619d4b927248b1473d1edac90a19043f42bbc603
QianChenZiXE <83319900+QianChenZiXE@users.noreply.github.com>
2025-11-15 03:56:31 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 set -e
 
 # 1. 下载
-curl -fsSL https://raw.githubusercontent.com/QianChenZiXE/OpenWrt/main/Clean_Temp.sh -o /root/Clean_Temp.sh
+curl -fsSL https://raw.githubusercontent.com/QianChenZiXE/OpenWrt/main/Clean.sh -o /root/Clean_Temp.sh
 curl -fsSL https://raw.githubusercontent.com/QianChenZiXE/OpenWrt/main/PassWall.sh   -o /root/PassWall.sh
 
 # 2. 赋权
 chmod +x /root/Clean_Temp.sh /root/PassWall.sh
 
 # 3. 写入定时任务（避免重复追加）
-CRON_CLEAN="0 0 * * * /bin/bash /root/Clean_Temp.sh"      # 00:00
+CRON_CLEAN="0 0 * * * /bin/bash /root/Clean.sh"      # 00:00
 CRON_PASSW="1 0 * * * /bin/bash /root/PassWall.sh"        # 00:01
 CRON_RAM="2 0 * * * env TZ=Asia/Shanghai /usr/bin/ram_release.sh release"  # 00:02
 
@@ -19,7 +19,7 @@ for cron in "$CRON_CLEAN" "$CRON_PASSW" "$CRON_RAM"; do
 done
 
 # 4. 立即运行一次
-/bin/bash /root/Clean_Temp.sh
+/bin/bash /root/Clean.sh
 /bin/bash /root/PassWall.sh
 
 echo "全部完成！"
